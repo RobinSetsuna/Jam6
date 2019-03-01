fab493f1051c229f6781122618502c409adae905
robinsetsuna <robinoneword@outlook.com>
2019-03-01 18:39:19 -0500

    Create Unity Project

--- src/Jam6/Assets/Scripts/Utility/Events.cs
+++
@@ -1,4 +0,0 @@
-﻿using UnityEngine.Events;
-
-public class EventOnDataUpdate<T> : UnityEvent<T> { }           // EventHandler(T current)
-public class EventOnDataChange<T> : UnityEvent<T, T> { }        // EventHandler(T current, T previous)
