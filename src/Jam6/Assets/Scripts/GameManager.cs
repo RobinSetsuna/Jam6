9ace7b66f10cd3941842a6e2032c1259e9fad0a9
Zheyu Zhang <eric.zhangzheyu@gmail.com>
2019-03-01 15:55:34 -0500

    Initial setup

---
+++ src/Jam6/Assets/Scripts/GameManager.cs
@@ -0,0 +1,115 @@
+﻿/**
+ * @author SerapH
+ */
+
+using UnityEngine;
+
+/// <summary>
+/// The state of the game
+/// </summary>
+public enum GameState : int
+{
+    Start = 1,
+}
+
+/// <summary>
+/// A FSM for the whole game at the highest level
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    /// <summary>
+    /// The unique instance
+    /// </summary>
+    public static GameManager Singleton { get; private set; }
+
+    /// <summary>
+    /// An event triggered whenever the state of the game changes
+    /// </summary>
+    public EventOnDataChange<GameState> OnCurrentGameStateChange { private set; get; }
+
+    [SerializeField] private GameState initialState = GameState.Start;
+
+    private GameState currentGameState;
+
+    /// <summary>
+    /// The current state of the game
+    /// </summary>
+    public GameState CurrentGameState
+    {
+        get
+        {
+            return currentGameState;
+        }
+
+        private set
+        {
+
+
+            // Reset the current state
+            if (value == currentGameState)
+            {
+#if UNITY_EDITOR
+                LogUtility.PrintLogFormat("GameManager", "Reset {0}.", value);
+#endif
+
+                //switch (currentGameState)
+                //{
+                //}
+            }
+            else
+            {
+                // Before leaving the previous state
+                //switch (currentGameState)
+                //{
+                //}
+
+#if UNITY_EDITOR
+                LogUtility.PrintLogFormat("GameManager", "Made a transition to {0}.", value);
+#endif
+
+                GameState previousGameState = CurrentGameState;
+                currentGameState = value;
+
+                // After entering the new state
+                //switch (currentGameState)
+                //{
+                //}
+
+                OnCurrentGameStateChange.Invoke(previousGameState, currentGameState);
+
+                //switch (currentGameState)
+                //{
+                //}
+            }
+        }
+    }
+
+    private GameManager() { }
+
+    /// <summary>
+    /// Quit the game
+    /// </summary>
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private void Awake()
+    {
+        if (!Singleton)
+        {
+            Singleton = this;
+            DontDestroyOnLoad(gameObject);
+
+            OnCurrentGameStateChange = new EventOnDataChange<GameState>();
+        }
+        else if (this != Singleton)
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        CurrentGameState = initialState;
+    }
+}
+
