880aecbb35deb77e4f56ff8c525d4e5d98085094
riverluara <riverphoebe@163.com>
2019-03-03 13:21:09 -0500

    Speedup small heal shield(without completion)

--- src/Jam6/Assets/Scripts/ItemDataManager.cs
+++ src/Jam6/Assets/Scripts/ItemDataManager.cs
@@ -6,15 +6,21 @@ public class ItemDataManager : MonoBehaviour {
     [SerializeField] private float playerHealth = 10.0f;
     public float maxHealth = 15.0f;
     public float intervalOfHealOBj = 5.0f;
+    public float intervalOfSpeedUpObj = 5.0f;
+    public float intervalOfSmallObj = 5.0f;
+    public float intervalOfShieldObj = 5.0f;
+    //public GameObject healObjPrefab;
+
 
     private float xConstraint = 4.38f;
     private float yConstraint = 5.5f;
-    private float previousTime;
+    private float previousTimeHeal;
+    private float previousSpeedUp;
+    private float previousSmall;
+    private float previousShield;
 
-    public GameObject healObjPrefab;
-    
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         //
         GameObject HealObj = ObjectPool.GetInstance().GetObj("HealObj", new Vector3(Random.Range((-1.0f) * xConstraint, xConstraint), yConstraint, 0.0f));
 
@@ -23,23 +29,72 @@ public class ItemDataManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if((Time.time - previousTime)> intervalOfHealOBj)
+		if((Time.time - previousTimeHeal)> intervalOfHealOBj)
         {
             InstantiateHealObj();
-            previousTime = Time.time;
+            previousTimeHeal = Time.time;
+        }
+        if ((Time.time - previousSpeedUp) > intervalOfSpeedUpObj)
+        {
+            InstantiateSpeedUpObj();
+            previousSpeedUp = Time.time;
+        }
+        if ((Time.time - previousSmall) > intervalOfSmallObj)
+        {
+            InstantiateSmallObj();
+            previousSmall = Time.time;
+        }
+        if ((Time.time - previousShield) > intervalOfShieldObj)
+        {
+            InstantiateShieldObj();
+            previousShield = Time.time;
         }
     }
-    public void AddHealth(Collision2D collision)
+    public void AddHealth(Collider2D collision)
     {
         if(playerHealth < maxHealth) {
             playerHealth++;
             ObjectPool.GetInstance().RecycleObj(collision.gameObject);
         }
     }
+    public void SpeedUp(Collider2D collision)
+    {
+        ObjectPool.GetInstance().RecycleObj(collision.gameObject);
+    }
+    public void BecomeSmall(Collider2D collision) 
+    {
+        ObjectPool.GetInstance().RecycleObj(collision.gameObject);
+
+    }
+    public void WithShield(Collider2D collision)
+    {
+        ObjectPool.GetInstance().RecycleObj(collision.gameObject);
+    }
+
     void InstantiateHealObj()
     {
         GameObject HealObjPrefab = ObjectPool.GetInstance().GetObj("HealObj", new Vector3(Random.Range((-1.0f) * xConstraint, xConstraint), yConstraint, 0.0f));
         //HealObjPrefab.transform.position = new Vector3(Random.Range((-1.0f) * xConstraint, xConstraint), yConstraint, 0.0f);
       
     }
+    void InstantiateSpeedUpObj()
+    {
+        GameObject HealObjPrefab = ObjectPool.GetInstance().GetObj("SpeedUpObj", new Vector3(Random.Range((-1.0f) * xConstraint, xConstraint), yConstraint, 0.0f));
+
+    }
+    void InstantiateSmallObj()
+    {
+        GameObject HealObjPrefab = ObjectPool.GetInstance().GetObj("SmallObj", new Vector3(Random.Range((-1.0f) * xConstraint, xConstraint), yConstraint, 0.0f));
+
+    }
+    void InstantiateShieldObj()
+    {
+        GameObject HealObjPrefab = ObjectPool.GetInstance().GetObj("SheildObj", new Vector3(Random.Range((-1.0f) * xConstraint, xConstraint), yConstraint, 0.0f));
+
+    }
+
+
+
+
+
 }
--- src/Jam6/Assets/Scripts/PlayerMovement.cs
+++ src/Jam6/Assets/Scripts/PlayerMovement.cs
@@ -2,20 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
     public float speed = 2.0f;
     public ItemDataManager dataManager;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Move();
+    public float fireInterval = 2.0f;
+    public float speedUpTime = 3.0f;
+    public float smallInterval = 3.0f;
+    public float shieldInterval = 3.0f;
+    public GameObject bullet;
+    private float previousTime;
+    private bool isSmall = false;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        previousTime = Time.time;
     }
-   void Move()
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        if ((Time.time - previousTime) > fireInterval)
+        {
+            Shooting();
+            previousTime = Time.time;
+        }
+    }
+    void Move()
     {
 
         if (Input.GetKey(KeyCode.W))
@@ -35,7 +53,8 @@ public class PlayerMovement : MonoBehaviour {
             this.transform.position += Vector3.right * speed * Time.deltaTime;
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         //Detect if collision of items happen
         Debug.Log("Collision:" + collision.gameObject.tag.ToString());
@@ -44,5 +63,56 @@ public class PlayerMovement : MonoBehaviour {
             Debug.Log("Collision:" + collision.gameObject.tag.ToString());
             dataManager.AddHealth(collision);
         }
+        else if (collision.gameObject.tag.Equals("SpeedUpObj"))
+        {
+            Debug.Log("Collision:" + collision.gameObject.tag.ToString());
+            dataManager.SpeedUp(collision);
+            StartCoroutine(SpeedUpFireInterval());
+
+        }
+        else if (collision.gameObject.tag.Equals("SmallObj"))
+        {
+            Debug.Log("Collision:" + collision.gameObject.tag.ToString());
+            dataManager.BecomeSmall(collision);
+            StartCoroutine(BecomeSmaller());
+        }
+        else if (collision.gameObject.tag.Equals("SmallObj"))
+        {
+
+            Debug.Log("Collision:" + collision.gameObject.tag.ToString());
+            dataManager.WithShield(collision);
+            StartCoroutine(ArmWithShield());
+        }
+    }
+    void Shooting()
+    {
+        GameObject BulletPrefab = ObjectPool.GetInstance().GetObj("Bullet", this.transform.position);
+
+    }
+
+    IEnumerator SpeedUpFireInterval()
+    {
+        fireInterval = 0.3f;
+        yield return new WaitForSeconds(speedUpTime);
+        fireInterval = 2.0f;
+    }
+    IEnumerator BecomeSmaller()
+    {
+        if (!isSmall)
+        {
+            isSmall = true;
+            this.transform.localScale = this.transform.localScale * 0.5f;
+            yield return new WaitForSeconds(smallInterval);
+            this.transform.localScale = this.transform.localScale * 2.0f;
+            isSmall = false;
+        }
+
+    }
+    IEnumerator ArmWithShield()
+    {
+        //Bool -> isShield with isShield = flase, the enememy can damage player
+        Debug.Log("Shield");
+        yield return new WaitForSeconds(shieldInterval);
+        Debug.Log("No Shield");
     }
 }
