79b923b9d3388e25e56c38084d9712759098fad4
@sb5411 <sb5411@rit.edu>
2019-03-03 21:27:33 -0500

    Health and Weapon UI

---
+++ src/Jam6/Assets/Scripts/Health.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Health : MonoBehaviour {
+
+    public int health;
+    public int numOfHealth;
+
+
+    public Image[] myhealthbars;
+    public Sprite full;
+    public Sprite empty;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (health > numOfHealth)
+        {
+            health = numOfHealth;
+        }
+
+        for (int i = 0; i < myhealthbars.Length; i++)
+        {
+            if (i < health)
+            {
+                myhealthbars[i].sprite = full;
+            }
+            else
+            {
+                myhealthbars[i].sprite = empty;
+            }
+
+            if(i<numOfHealth)
+            {
+                myhealthbars[i].enabled = true;
+            }
+            else
+            {
+                myhealthbars[i].enabled = false;
+            }
+        }
+		
+	}
+}
