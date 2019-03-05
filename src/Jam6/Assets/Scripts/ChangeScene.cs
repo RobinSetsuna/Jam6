34fd6e11d8d23ed98f87bcd8963ff86e03f46766
@sb5411 <sb5411@rit.edu>
2019-03-05 04:33:06 -0500

    Audio&Credits

---
+++ src/Jam6/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour {
+    public void  Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
